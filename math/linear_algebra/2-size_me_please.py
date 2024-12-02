40d8fdc423e7be5793c7c2bc79c6e2be1e7eed65
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-02 13:30:38 +0100

    2. Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    '''fonction qui donne la taille d'une matrice'''
     for i in range(len(matrix)):
         if type(matrix[i]) is list:
             return [len(matrix)] + matrix_shape(matrix[i])
