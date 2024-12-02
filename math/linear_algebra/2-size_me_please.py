3026246d09e246d7068843596b2b9a8b7925b6c2
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-02 13:29:07 +0100

    2. Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    for i in range (len(matrix)):
+    for i in range(len(matrix)):
         if type(matrix[i]) is list:
             return [len(matrix)] + matrix_shape(matrix[i])
         else:
