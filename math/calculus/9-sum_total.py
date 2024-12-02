338f64ac2b1af2f3a9637f905916b4d2cf2d8e06
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-02 15:15:10 +0100

    9. Our life is the sum total of all the decisions we make every day, and those decisions are determined by our priorities

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""This module contains a function to calculate\\
+    the sum of squares of the first n natural numbers."""
+
 
 def summation_i_squared(n):
+    """
+    Calculates the sum of squares of the first n natural numbers.
+
+    Args:
+        n (int): The number up to which the sum of squares is to be calculated.
+
+    Returns:
+        int: The sum of squares of the first n natural numbers,\\
+            or None if n is less than 1.
+    """
     if n < 1:
         return None
     else:
