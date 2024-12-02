e4a99f06589ae6d6d71494c3d07123309fd99c25
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-02 15:51:35 +0100

    10. Derive happiness in oneself from a good day's work

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -14,7 +14,7 @@ def poly_derivative(poly):
         list: A list of coefficients representing
         the derivative of the polynomial.
     """
-    if not isinstance(poly, list):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
     if len(poly) == 1:
         return [0]
