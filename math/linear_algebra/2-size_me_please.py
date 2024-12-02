e252424e7d87a907af8c3be11c4216213eb9f56b
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-02 13:39:15 +0100

    2. Size Me Please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
+"""
+This module contains a function to determine the shape of a matrix.
+"""
 
 
 def matrix_shape(matrix):
-    """Retourne la dimension d'une matrice sous forme de liste"""
+    """
+    Returns the dimensions of a matrix as a list.
+
+    Args:
+        matrix (list): A list of lists representing the matrix.
+
+    Returns:
+        list: A list representing the dimensions of the matrix.
+    """
     for i in range(len(matrix)):
         if type(matrix[i]) is list:
             return [len(matrix)] + matrix_shape(matrix[i])
