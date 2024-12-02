a8bd5e0220b191f72838194874fce10d24372773
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-02 15:31:00 +0100

    10. Derive happiness in oneself from a good day's work

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -3,8 +3,17 @@
 
 
 def poly_derivative(poly):
-    deriv=[]
-    for i in range(1,len(poly)):
+    """
+    Calculates the derivative of a polynomial.
+
+    Args:
+        poly (list): A list of coefficients representing the polynomial. The index of each element represents the power of x.
+
+    Returns:
+        list: A list of coefficients representing the derivative of the polynomial.
+    """
+    deriv = []
+    for i in range(1, len(poly)):
         poly[i] *= i
         deriv.append(poly[i])
     return deriv
