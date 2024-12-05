4dd91d64f3029d446796a63315e963a6d4eef5fe
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-05 11:28:31 +0100

    4. Frequency

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -11,10 +11,11 @@ def frequency():
     plt.figure(figsize=(6.4, 4.8))
 
     plt.ylim(0, 30)
-    plt.xlim(0, 100)
+
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title('Project A')
-    plt.hist(student_grades, bins=10, edgecolor='k')
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='k')
     plt.xticks(np.arange(0, 101, 10))
+    plt.xlim(0, 100)
     plt.savefig('frequency.png')
