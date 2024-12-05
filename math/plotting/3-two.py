9a8a62c7685eba8eff4a8ad06fb58575e05f8dc9
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-05 10:49:46 +0100

    3. Two is better than one

---
+++ math/plotting/3-two.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""3. Two is better than one"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """Trace la radioctivité de 2 éléments en fonction du temps"""
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.plot(x, y1, linestyle='--', c='r', label='C-14')
+    plt.plot(x, y2, c='g', label='Ra-226')
+    plt.legend(loc='upper right')
+    plt.savefig('decay_of_two_elements.png')
