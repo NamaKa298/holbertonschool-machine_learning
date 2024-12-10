7e629e00d4b4010e78eb3f03abe4d8363948df13
NamaKa298 <marion.saint-martin@protonmail.com>
2024-12-10 15:15:13 +0100

    17. Squashed Like Sardines

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -8,18 +8,21 @@ def cat_matrices(mat1, mat2, axis=0):
     if not isinstance(mat1, list) or not isinstance(mat2, list):
         return None
 
-    # Si axis == 0, concatène directement les listes principales
     if axis == 0:
+        # Vérifier que toutes les sous-listes (lignes) de mat1 et
+        # mat2 ont la même longueur
+        if any(len(row) != len(mat1[0]) for row in mat1 + mat2):
+            return None
         return mat1 + mat2
 
-    # Si axis > 0, gérer la concaténation récursive
     if len(mat1) != len(mat2):
         return None
 
     new_mat = []
     for sub1, sub2 in zip(mat1, mat2):
-        concatenated = cat_matrices(sub1, sub2, axis=axis-1)
+        concatenated = cat_matrices(sub1, sub2, axis - 1)
         if concatenated is None:
             return None
         new_mat.append(concatenated)
+
     return new_mat
